ee2128e917fadcf803330b0c8697c8ed9f7cb84d
CREDO23 <thierrybakera12@gmail.com>
2023-09-03 22:22:16 +0200

    feat: add new Shop form

---
+++ src/components/shop/forms/generalInfo/index.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import Input from "@/components/global/input";
+import Select from "@/components/global/select";
+import { Controller, useForm } from "react-hook-form";
+import UploadImage from "../../../global/image/imageUpload";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { useState } from "react";
+import Button from "@/components/global/button";
+
+export default function GeneralInfoForm() {
+  const { handleSubmit, control } = useForm();
+
+  const [collapse, setCollapse] = useState(false);
+
+  return (
+    <div
+      className={` overflow-hidden rounded-md border border-slate-400/80 transition-all w-full ${
+        collapse ? "h-10" : "h-auto"
+      } `}
+    >
+      <div
+        onClick={() => setCollapse(!collapse)}
+        className="w-full cursor-pointer  border-b h-10 px-2 flex items-center justify-between"
+      >
+        <h3 className=" font-medium flex items-center gap-4">
+          <span className="h-4 w-4 rounded-full border bg-primary-color border-primary-color"></span>{" "}
+          <span>General Information</span>
+        </h3>
+        <span className=" text-lg">
+          {collapse ? <IoIosArrowDown /> : <IoIosArrowUp />}
+        </span>
+      </div>
+      <form className=" w-full" action="">
+        <div className="flex p-2 w-full flex-col flex-wrap  gap-4">
+          <div className="w-full  gap-4 flex flex-wrap">
+            <div className="w-80 shrink-0">
+              <Controller
+                name="shopName"
+                control={control}
+                render={({
+                  field: { ref, ...field },
+                  fieldState: { error },
+                }) => (
+                  <Input
+                    label={<p className=" text-sm">Shop name</p>}
+                    placeholder="Enter the shop name"
+                    type="text"
+                    refEl={ref}
+                    {...field}
+                    error={error?.message}
+                  />
+                )}
+              />
+            </div>
+            <div className="w-80 shrink-0">
+              <Controller
+                name="shopCategory"
+                control={control}
+                render={({
+                  field: { ref, ...field },
+                  fieldState: { error },
+                }) => (
+                  <Select
+                    label={<p className=" text-sm">Category</p>}
+                    placeholder="Select a Category"
+                    options={[{ label: "credo", value: 34 }]}
+                  />
+                )}
+              />
+            </div>
+            <div className="w-80 shrink-0">
+              <Controller
+                name="shopDescription"
+                control={control}
+                render={({
+                  field: { ref, ...field },
+                  fieldState: { error },
+                }) => (
+                  <Input
+                    label={<p className=" text-sm">Shop description</p>}
+                    placeholder="Describe the shop"
+                    type="textarea"
+                    refEl={ref}
+                    {...field}
+                    error={error?.message}
+                  />
+                )}
+              />
+            </div>
+          </div>
+          <div className="w-full flex items-center">
+            <div className="flex flex-col gap-1">
+              <span className=" text-sm">Product image</span>
+              <UploadImage quantity={1} />
+            </div>
+          </div>
+        </div>
+        <div className="w-full border-t p-2 flex items-center justify-end">
+          <Button title="Save" />
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ src/components/shop/forms/paymentMethods/index.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { Controller, useForm } from "react-hook-form";
+import { Checkbox, Row, Col } from "antd";
+import Button from "@/components/global/button";
+
+export default function PaymentMethodsForm() {
+  const [collapse, setCollapse] = useState(false);
+  const { handleSubmit, control } = useForm();
+
+  return (
+    <div
+      className={` overflow-hidden rounded-md border border-slate-400/80 transition-all w-full ${
+        collapse ? "h-10" : "h-auto"
+      } `}
+    >
+      <div
+        onClick={() => setCollapse(!collapse)}
+        className="w-full cursor-pointer  border-b h-10 px-2 flex items-center justify-between"
+      >
+        <h3 className=" font-medium flex items-center gap-4">
+          <span className="h-4 w-4 rounded-full border bg-primary-color border-primary-color"></span>{" "}
+          <span>Payment methods</span>
+        </h3>
+        <span className=" text-lg">
+          {collapse ? <IoIosArrowDown /> : <IoIosArrowUp />}
+        </span>
+      </div>
+      <form className=" w-full" action="">
+        <div className="flex p-2 w-full flex-col flex-wrap  gap-4">
+      <Controller
+        name="shopName"
+        control={control}
+        render={({ field: { ref, ...field }, fieldState: { error } }) => (
+          <Checkbox.Group style={{ width: "100%" }} {...field}>
+            <Row>
+              <Col span={8}>
+                <Checkbox value="Airtel Money">Airtel Money</Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="Orange Money">Orange Money</Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="M-pesa">M-pesa</Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="Bitcoin">Bitcoin</Checkbox>
+              </Col>
+            </Row>
+          </Checkbox.Group>
+        )}
+      />
+         </div>
+         <div className="w-full border-t p-2 flex items-center justify-end">
+          <Button title="Save" />
+        </div>
+      </form>
+    </div>
+  );
+}
